d60da2d1f678f68bde1b53c4894b19d97a2b2561
vu <dtvu1707@gmail.com>
2023-07-24 06:43:29 +0000

    update

--- Capsule/CapsuleLayer.py
+++ Capsule/CapsuleLayer.py
@@ -233,10 +233,10 @@ class AdaptiveCapsuleHead(nn.Module):
             p = p.reshape(b, d // (self.P ** 2), self.P ** 2, h, w)
        
         p_out, a_out = self.routinglayer(p, a)
-        # a_out = torch.log(a_out / (1 - a_out + EPS))
         if self.reduce:
             p_out, a_out = self.capspooling(p_out, a_out)
        
+        a_out = torch.log(a_out / (1 - a_out + EPS))
         if get_capsules:
             return p_out, a_out
         else: 
